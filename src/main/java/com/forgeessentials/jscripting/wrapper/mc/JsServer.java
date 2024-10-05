8829ef65cde330aa7c1390a572ac21f2ecd3dff8
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2024-10-05 14:32:48 -0500

    Fix AFK player spamming (#2696)
    * Fix AFK player spamming
    
    * Fix space missing in command parser
    
    ---------
    
    Co-authored-by: Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>

--- src/main/java/com/forgeessentials/jscripting/wrapper/mc/JsServer.java
+++ src/main/java/com/forgeessentials/jscripting/wrapper/mc/JsServer.java
@@ -19,8 +19,9 @@ import com.mojang.brigadier.ParseResults;
 
 import net.minecraft.command.CommandException;
 import net.minecraft.command.CommandSource;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.Util;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
@@ -79,7 +80,7 @@ public class JsServer
 
         // Join and split again to fix invalid arguments containing spaces
         String cmdLine = StringUtils.join(strArgs, " ");
-        cmd = cmd + cmdLine;
+        cmd = cmd + " " + cmdLine;
 
         MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
         final ParseResults<CommandSource> command = (ParseResults<CommandSource>) server.getCommands().getDispatcher()
@@ -226,9 +227,9 @@ public class JsServer
         {
             ITextComponent component = ITextComponent.Serializer.fromJson(msg);
 
-            for (PlayerEntity p : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers())
+            for (ServerPlayerEntity p : server.getPlayerList().getPlayers())
             {
-                server.getPlayerList().broadcastMessage(component, ChatType.CHAT, p.getGameProfile().getId());
+            	p.sendMessage(component, ChatType.CHAT, Util.NIL_UUID);
             }
         }
         catch (JsonParseException jsonparseexception)
